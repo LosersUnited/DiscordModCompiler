03faf97e11216ae391c2f6f7bd5021e113ff4b82
Daniel <davilareko@gmail.com>
2024-02-05 01:13:10 +0100

    Cleanup cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -6,28 +6,15 @@ import converter from "./converter.js";
 import { File } from "@babel/types";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-// if (process.argv.includes("--cli")) {
 const supportedClientMods = [...readdirSync(__dirname + "/converters"), "all"];
 const code = readFileSync(process.argv[2], "utf8");
 const ast = parse(code, { sourceType: "module" });
 
-// const output = generate(
-//     ast,
-//     {
-//         /* options */
-//     },
-//     // code
-// );
-// console.log(ast, output);
 const targetDiscordMod = process.argv[3] + ".js";
 const supported = (sample: string) => supportedClientMods.indexOf(sample) != -1;
 if (supported(targetDiscordMod)) {
     const filler = import(__dirname + "/converters/" + targetDiscordMod);
     filler.then((x: any) => {
-        // const test = x.WebpackApi.getModule; // this has to be this way, kinda universal
-        // console.log(test);
-        // ast.program.body
-        debugger;
         const out = converter(ast);
         const outMod = {
             ...ast,
@@ -40,4 +27,3 @@ if (supported(targetDiscordMod)) {
         console.log(generate(outMod));
     });
 }
-// }
