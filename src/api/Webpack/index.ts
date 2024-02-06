17643f7389840cc6e313e6c1688ed11fb41f4626
Daniel <davilareko@gmail.com>
2024-02-06 17:45:29 +0100

    Update index.ts

--- src/api/Webpack/index.ts
+++ src/api/Webpack/index.ts
@@ -3,7 +3,8 @@ interface base_WebpackApi {
 }
 export default base_WebpackApi;
 class WebpackApi_c implements base_WebpackApi {
-    getModule() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    getModule(filter: (match: any) => boolean) {
         throw new Error("Method not implemented. This is a dummy class.");
     }
 }
