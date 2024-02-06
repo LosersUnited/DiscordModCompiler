07d5971c8e0009f3a52a968de0caa64c5d834250
Daniel <davilareko@gmail.com>
2024-02-06 17:46:27 +0100

    Remove destructuring from input code

--- src/cli.ts
+++ src/cli.ts
@@ -5,19 +5,29 @@ import * as url from 'url';
 import converter from "./converter.js";
 import { File } from "@babel/types";
 import { myPackageName } from "./utils.js";
+import { transformSync } from "@babel/core";
 if (process.argv.length == 4) {
     const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
     const supportedClientMods = [...readdirSync(`${__dirname}/converters`), "all"];
     const code = readFileSync(process.argv[2], "utf8");
-    const ast = parse(code, { sourceType: "module" });
-
+    const codeMod = transformSync(code, { plugins: ["@babel/plugin-transform-destructuring", { "useBuiltIns": true }] })?.code;
+    if (!codeMod) {
+        process.exit(1);
+    }
+    console.log("codeMod:", codeMod);
+    const ast = parse(codeMod, { sourceType: "module" });
+    // const ast = transformSync(code, { plugins: ["@babel/plugin-transform-destructuring"], sourceType: "module" })?.ast;
+    // if (!ast) {
+    //     process.exit(1);
+    // }
+    // console.log(ast.errors);
     const targetDiscordMod = process.argv[3] + ".js";
     const supported = (sample: string) => supportedClientMods.indexOf(sample) != -1;
     if (supported(targetDiscordMod)) {
         const filler = import(url.pathToFileURL(`${__dirname}/converters/${targetDiscordMod}`).href);
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         filler.then((x: any) => {
-            const out = converter(ast);
+            const out = converter(ast as File & { errors: [] }, x);
             const outMod = {
                 ...ast,
                 program: {
--- test/sample/index.js
+++ test/sample/index.js
@@ -2,5 +2,9 @@ import { WebpackApi, pathToFunction } from "discord-mod-compiler";
 import { readFileSync } from "fs";
 // import { pathToFunction } from "discord-mod-compiler";
 function start() {
-    const someModule = WebpackApi.getModule(x=>x._sendMessage);
+    // const getModule = WebpackApi.getModule;
+
+    const { getModule } = WebpackApi;
+    // const someModule = WebpackApi.getModule(x => x._sendMessage);
+    const someModule = getModule(x => x._sendMessage);
 }
