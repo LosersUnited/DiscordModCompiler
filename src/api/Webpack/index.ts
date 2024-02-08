9a6c9cd2c0603a9b57ed4bb402bf1af6bcaf194f
JustOptimize <mirko303.staff@gmail.com>
2024-02-08 12:08:27 +0100

    refactor(webpack): Renamed Dummy Class

--- src/api/Webpack/index.ts
+++ src/api/Webpack/index.ts
@@ -2,10 +2,12 @@ interface IBaseWebpackApi {
     getModule(filter: (match: any) => boolean): any;
 }
 export default IBaseWebpackApi;
-class WebpackApi_c implements IBaseWebpackApi {
+
+class DummyWebpackApi implements IBaseWebpackApi {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     getModule(filter: (match: any) => boolean) {
         throw new Error("Method not implemented. This is a dummy class.");
     }
 }
-export const WebpackApi = new WebpackApi_c(); // ts stupid
+
+export const WebpackApi = new DummyWebpackApi();
