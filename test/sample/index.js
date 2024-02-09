0f0125da8b3a9755c9d0a70e32731447a503b99c
JustOptimize <mirko303.staff@gmail.com>
2024-02-09 10:20:58 +0100

    Added tests

--- test/sample/index.js
+++ test/sample/index.js
@@ -1,10 +1,25 @@
-import { WebpackApi, pathToFunction } from "discord-mod-compiler";
-import { readFileSync } from "fs";
-// import { pathToFunction } from "discord-mod-compiler";
-function start() {
-    // const getModule = WebpackApi.getModule;
+import { WebpackApi } from "discord-mod-compiler";
 
-    const { getModule } = WebpackApi;
-    // const someModule = WebpackApi.getModule(x => x._sendMessage);
-    const someModule = getModule(x => x._sendMessage);
+function start() {
+    const getModuleType1 = WebpackApi.getModule;
+    const someModuleType1 = getModule(x => x._sendMessage);
+
+    const { getModule: getModuleType2 } = WebpackApi;
+    const someModuleType2 = getModuleType2(x => x._sendMessage);
+    
+    const someModuleType3 = WebpackApi.getModule(x => x._sendMessage);
+
+    const modules = [
+        getModuleType1,
+        someModuleType1,
+        getModuleType2,
+        someModuleType2,
+        someModuleType3
+    ]
+
+    if (modules.any(x => x === undefined)) {
+        throw new Error("WebpackApi.getModule is not working");
+    } else {
+        console.log("WebpackApi.getModule is working");
+    }
 }
