4da022d4c5ff272ad49a31e3b50e6edd05fabaf0
JustOptimize <mirko303.staff@gmail.com>
2024-02-09 15:54:23 +0100

    formatted

--- src/converters/betterdiscord.ts
+++ src/converters/betterdiscord.ts
@@ -1,9 +1,11 @@
 import { IBaseWebpackApi, createFunctionFromObjectProperty } from "../api/index.js";
+
 class BDWebpackApi implements IBaseWebpackApi {
     get getModule() {
         return createFunctionFromObjectProperty("BdApi.Webpack", "getModule");
     }
 }
+
 export default {
     WebpackApi: new BDWebpackApi(),
 };
--- src/converters/replugged.ts
+++ src/converters/replugged.ts
@@ -1,9 +1,11 @@
 import { IBaseWebpackApi, createFunctionFromObjectProperty } from "../api/index.js";
+
 class RPWebpackApi implements IBaseWebpackApi {
     get getModule() {
         return createFunctionFromObjectProperty("replugged.webpack", "getModule");
     }
 }
+
 export default {
     WebpackApi: new RPWebpackApi(),
 };
