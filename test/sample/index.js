2e90f37fa225a0cf5a1950a347de93744efeb035
JustOptimize <mirko303.staff@gmail.com>
2024-02-14 10:33:29 +0100

    Formatted

--- test/sample/index.js
+++ test/sample/index.js
@@ -12,23 +12,23 @@ module.exports = class MyPlugin {
         // Do stuff in here before starting
     }
 
-     start() {
+    start() {
         const getModuleType1 = WebpackApi.getModule;
-        const someModuleType1 = getModuleType1(x => x._sendMessage);
-    
+        const someModuleType1 = getModuleType1((x) => x._sendMessage);
+
         const { getModule: getModuleType2 } = WebpackApi;
-        const someModuleType2 = getModuleType2(x => x._sendMessage);
-        
-        const someModuleType3 = WebpackApi.getModule(x => x._sendMessage);
-    
+        const someModuleType2 = getModuleType2((x) => x._sendMessage);
+
+        const someModuleType3 = WebpackApi.getModule((x) => x._sendMessage);
+
         const modules = [
             getModuleType1,
             someModuleType1,
             getModuleType2,
             someModuleType2,
-            someModuleType3
-        ]
-    
+            someModuleType3,
+        ];
+
         if (modules.includes(undefined)) {
             throw new Error("WebpackApi.getModule is not working");
         } else {
