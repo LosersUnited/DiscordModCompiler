ce9a6b9499a4e80a877bd7908476dde7f7d872d1
Daniel <davilareko@gmail.com>
2024-02-22 18:27:08 +0100

    Fix sample file

--- test/sample/index.js
+++ test/sample/index.js
@@ -2,11 +2,11 @@ import { WebpackApi } from "discord-mod-compiler";
 
 function start() {
     const getModuleType1 = WebpackApi.getModule;
-    const someModuleType1 = getModule(x => x._sendMessage);
+    const someModuleType1 = getModuleType1(x => x._sendMessage);
 
     const { getModule: getModuleType2 } = WebpackApi;
     const someModuleType2 = getModuleType2(x => x._sendMessage);
-    
+
     const someModuleType3 = WebpackApi.getModule(x => x._sendMessage);
 
     const modules = [
@@ -17,9 +17,15 @@ function start() {
         someModuleType3
     ]
 
-    if (modules.any(x => x === undefined)) {
+    if (modules.some(x => x === undefined)) {
         throw new Error("WebpackApi.getModule is not working");
     } else {
         console.log("WebpackApi.getModule is working");
     }
 }
+
+function stop() {
+
+}
+
+export { start, stop };
